defd51f37cb631312b208743992882b2664870eb
Soban Abbas <sobanabbass@gmail.com>
2025-11-15 14:02:15 +0500

    learinput, print , datatype, typecasting,type etc

---
+++ Code/Basics/ch2_prct.py
@@ -0,0 +1,24 @@
+a="soban"
+print(a)
+b=2
+print(b)
+c=True
+print(c)
+# its automaticallly identiy type oif data during assingment to veriable
+
+# Operators in python
+b=+8
+print(b)
+b+=2
+print(b)
+# if we do b=-7 its means we are updaing value of b to -7
+b-=3
+print(b)
+
+b=float(b)
+print(type(b))
+
+number=input("find square of number")
+number=float(number)
+print(f"sqaure of number {number} is {number*number}")
+
