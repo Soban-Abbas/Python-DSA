69f0ef9d5ce919f5beef571ae3c6c6cf1a175c0f
Soban Abbas <sobanabbass@gmail.com>
2025-11-15 14:23:12 +0500

    chaper 2 questions

---
+++ Code/Basics/ch2_Ex.py
@@ -0,0 +1,28 @@
+#Write a python program to add two numbers.
+# number1=input("Enter 1st Number ")
+# number2=input("Enter 2nd Number ")
+# number1=int(number1)
+# number2=int(number2)
+# print(f"sum of number is {number1+number2}")
+
+
+# Write a python program to find remainder when a number is divided by z.
+# print("=========Find Remainder of Numbers==========")
+# num3=input("Enter 1st Number ")
+# num4=input("Enter 2st Number ")
+# print(type(num3))
+# num3=float(num3)
+# num4=float(num4)
+# remainder=num3%num4
+# print(f"remainder of numbers are {remainder}")
+
+
+# Use comparison operator to find out whether ‘a’ given variable a is greater than ‘b’ or not. Take a = 34 and b #= 80
+a=34
+b=80
+if a>b:
+    print("a is greater then b")
+else:
+    print("b is greater")    
+
+
